db603a3436db66ad19d57cee77ca0bfa472a802b
Michael Brooks <mikeyabrooks@gmail.com>
2017-11-18 10:39:29 -0800

    add updated swap of project to new repository

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117234222) do
+ActiveRecord::Schema.define(version: 20171118002301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20171117234222) do
     t.string "name"
     t.string "cost"
     t.string "origin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "author"
+    t.string "content"
+    t.integer "rating"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+50.times do |index|
+  Product.create(
+    name: (Faker::Food.ingredient).downcase!,
+    cost: Faker::Number.decimal(3, 2),
+    origin: (Faker::Address.country).downcase!
+    )
+end
+
+p "You now have #{Product.count} products in your db"
+
+Review.destroy_all
+
+250.times do |index|
+  Review.create(
+    author: (Faker::Name.name).downcase!,
+    content: Faker::Lorem.paragraph(3),
+    rating: Faker::Number.(1)
+    )
+end
+
+p "You now have #{Review.count} reviews in your db"
