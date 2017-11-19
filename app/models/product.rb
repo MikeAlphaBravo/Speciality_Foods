255eb93a59024b2bfe78fa03e2c8519ede016789
Michael Brooks <mikeyabrooks@gmail.com>
2017-11-18 16:27:16 -0800

    add scopes for 3 most recent and most reviewed not integrated to work yet

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,20 @@ class Product < ApplicationRecord
   validates :name, :cost, :origin, :presence => true
 
   scope :origin, -> (origin_parameter) { where(origin: origin_parameter) }
+  scope :recent, -> { order("created_at DESC").limit(3) }
   # scope :status, -> (status) { where status: status }
   # scope :location, -> (name) { where("name like ?", "#{name}%")}
   scope :index, -> (page) { order("name ASC").paginate(:page => page, :per_page => 10) }
   # scope :only_names, -> (name_parameter) { where("name like ?", "%#{name_parameter}%")}
   scope :search, -> (search) { where("name like ? OR cost like ? OR origin like ?", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%")}
+
+  scope :most_reviews, -> {(
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+  )}
 
   before_save :downcase_fields
 
